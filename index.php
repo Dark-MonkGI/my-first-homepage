cbf7c744f70e9d70fb8da67d84546ebb78dc40d2
Dark-MonkGI <gryaznov774@gmail.com>
2021-12-25 14:55:34 +0400

    initial project

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>GeekBrains Student Homepage</title>
+	<link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+
+<div class="content">
+	
+	<?php 
+	include 'menu.php';
+	?>
+
+	<h1>Личный сайт студента GeekBrains</h1>
+
+	<div class="center">
+	<img src="img/photo.png">
+		<div class="box_text">
+			<p><b>Приветствую!</b> Меня зовут <i>Илья</i>. Я начинающий программист и это мой первый сайт.</p>
+
+			<p>В обучении мне помог IT-портал <a href="https://geekbrains.ru">GeekBrains</a></p>
+
+			<p>Здесь вы можете поиграть в написанные мной игры, а так же сгенерировать себе пароль заданной длины: <br>
+			<a href="index.php">Главная,</a>
+			<a href="puzzle.php">Загадки,</a>
+			<a href="guess.php">Угадайка,</a>
+			<a href="generate.php">Генератор случайных паролей.</a>
+			</p>
+		</div>
+	</div>
+</div>
+<div class="footer">
+	Copyright &copy; <?php echo date ("y");?> Ilya_DM
+<div>
+
+
+</body>
+</html>
